f732e7c8717352e8df02569c0c8af614bba01ece
Ankur Pathak <ankurpathak@live.in>
2018-07-17 19:39:48 +0530

    "Start"

--- src/main/java/com/ankurpathak/springsessiontest/WebUtil.java
+++ src/main/java/com/ankurpathak/springsessiontest/WebUtil.java
@@ -7,6 +7,8 @@ import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.Locale;
+
 /**
  *
  *
@@ -25,6 +27,11 @@ public class WebUtil {
 
     public static boolean isAjax(SavedRequest request) {
         return request.getHeaderValues(X_REQUESTED_WITH).contains(XML_HTTP_REQUEST);
+    }
+
+
+    public static Locale getLocale(HttpServletRequest request){
+        return request.getLocale();
     }
 
 
