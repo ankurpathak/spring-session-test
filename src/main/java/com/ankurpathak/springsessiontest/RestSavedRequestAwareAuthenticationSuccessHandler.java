7df1d92b3867fd8b2d92f937930091d7b2a9d65c
Ankur Pathak <ankurpathak@live.in>
2018-10-24 05:42:01 +0530

    session control changes

--- src/main/java/com/ankurpathak/springsessiontest/RestSavedRequestAwareAuthenticationSuccessHandler.java
+++ src/main/java/com/ankurpathak/springsessiontest/RestSavedRequestAwareAuthenticationSuccessHandler.java
@@ -1,8 +1,8 @@
 package com.ankurpathak.springsessiontest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
@@ -15,26 +15,35 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Objects;
 
 
 @Component
 public class RestSavedRequestAwareAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-
+    public static final String DEFAULT_TARGET = "/api/me";
+    public static final String DEFAULT_TARGET_PARAMETER = "defaultTarget";
     private RequestCache requestCache = new HttpSessionRequestCache();
 
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
-    @Autowired
-    private MessageSource messageSource;
+    private final MessageSource messageSource;
+
+    public RestSavedRequestAwareAuthenticationSuccessHandler(ObjectMapper objectMapper, MessageSource messageSource) {
+        this.objectMapper = objectMapper;
+        this.messageSource = messageSource;
+        setRedirectStrategy(new DispatcherRedirectStrategy());
+        setAlwaysUseDefaultTargetUrl(true);
+        setDefaultTargetUrl(DEFAULT_TARGET);
+        setTargetUrlParameter(DEFAULT_TARGET_PARAMETER);
+    }
 
 
     @Override
     public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) throws ServletException, IOException {
         final SavedRequest savedRequest = requestCache.getRequest(request, response);
-
         if (savedRequest == null) {
+           // handle(request, response, authentication); //added for redirect
             clearAuthenticationAttributes(request);
             generateResponse(request, response);
             return;
@@ -42,6 +51,7 @@ public class RestSavedRequestAwareAuthenticationSuccessHandler extends SimpleUrl
         final String targetUrlParameter = getTargetUrlParameter();
         if (isAlwaysUseDefaultTargetUrl() || (targetUrlParameter != null && StringUtils.hasText(request.getParameter(targetUrlParameter)))) {
             requestCache.removeRequest(request, response);
+           // handle(request, response, authentication); //added for redirect
             clearAuthenticationAttributes(request);
             generateResponse(request, response);
             return;
@@ -50,11 +60,16 @@ public class RestSavedRequestAwareAuthenticationSuccessHandler extends SimpleUrl
         clearAuthenticationAttributes(request);
         generateResponse(request, response);
 
+        /*
 
         // Use the DefaultSavedRequest URL
-        // final String targetUrl = savedRequest.getRedirectUrl();
-        // logger.debug("Redirecting to DefaultSavedRequest Url: " + targetUrl);
-        // getRedirectStrategy().sendRedirect(request, response, targetUrl);
+        String targetUrl = getDefaultTargetUrl();
+        if(Objects.equals(savedRequest.getMethod(), HttpMethod.GET.name()))
+            targetUrl = savedRequest.getRedirectUrl();
+        logger.debug("Redirecting to DefaultSavedRequest Url: " + targetUrl);
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+
+        */
     }
 
     public void setRequestCache(final RequestCache requestCache) {
@@ -65,4 +80,6 @@ public class RestSavedRequestAwareAuthenticationSuccessHandler extends SimpleUrl
     private void generateResponse(HttpServletRequest request, HttpServletResponse response) throws IOException {
         FilterUtil.generateSuccess(response, objectMapper, messageSource);
     }
+
+
 }
