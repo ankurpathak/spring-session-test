29d4543d69df3835255d76b0200fca42d989d65f
Ankur Pathak <ankurpathak@live.in>
2018-10-24 05:39:48 +0530

    session control changes

---
+++ src/main/java/com/ankurpathak/springsessiontest/GetHttpServletRequestWrapper.java
@@ -0,0 +1,18 @@
+package com.ankurpathak.springsessiontest;
+
+import org.springframework.http.HttpMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class GetHttpServletRequestWrapper extends HttpServletRequestWrapper {
+    public GetHttpServletRequestWrapper(HttpServletRequest request) {
+        super(request);
+    }
+
+
+    @Override
+    public String getMethod() {
+        return HttpMethod.GET.name();
+    }
+}
