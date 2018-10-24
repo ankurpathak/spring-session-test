36508fbce88dd06c27ca9abde91000284090a8ff
Ankur Pathak <ankurpathak@live.in>
2018-10-24 05:39:02 +0530

    session control changes

--- src/main/java/com/ankurpathak/springsessiontest/CustomUserDetailsService.java
+++ src/main/java/com/ankurpathak/springsessiontest/CustomUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.ankurpathak.springsessiontest;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -8,7 +9,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
+import reactor.core.publisher.Mono;
+
 
 import java.util.*;
 
@@ -20,16 +22,21 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final IRoleService roleService;
     private final IUserService userService;
-    private final IpService ipService;
-    private final CountryCacheService countryService;
+    //private final IpService ipService;
+    //private final CountryCacheService countryService;
 
-    public CustomUserDetailsService(IRoleService roleService, IUserService userService, IpService ipService, CountryCacheService countryService) {
+    public CustomUserDetailsService(IRoleService roleService,
+                                    IUserService userService
+            //, IpService ipService,
+                                  //  CountryCacheService countryService
+    ) {
         this.roleService = roleService;
         this.userService = userService;
-        this.ipService = ipService;
-        this.countryService = countryService;
+        //this.ipService = ipService;
+       // this.countryService = countryService;
     }
 
+    /*
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         List<Criteria> criteriaList = new ArrayList<>();
@@ -61,8 +68,37 @@ public class CustomUserDetailsService implements UserDetailsService {
         }
     }
 
+    */
 
-    private Set<String> getPrivileges(Set<String> roles) {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = findByCandidateKey(username);
+        if (user.isPresent()) {
+            return CustomUserDetails.getInstance(user.get(), getPrivileges(user.get().getRoles()));
+        } else {
+            throw new UsernameNotFoundException(String.format(USERNAME_NOT_FOUND_MESSAGE, username));
+        }
+    }
+
+
+    public Optional<User> findByCandidateKey(String username){
+        List<Criteria> criteriaList = new ArrayList<>();
+        userService.possibleCandidateKeys(username)
+                .forEach((key, value) -> criteriaList.add(Criteria.where(KEY_MAPPINGS.get(key)).is(value)));
+
+        userService.possibleContacts(username)
+                .forEach(contact -> criteriaList.add(Criteria.where(KEY_CONTACT).is(contact)));
+
+        Criteria criteria = new Criteria();
+        if(CollectionUtils.isEmpty(criteriaList))
+            criteria.orOperator(criteriaList.toArray(new Criteria[]{}));
+        return userService.findByCriteria(criteria, PageRequest.of(0, 1), User.class)
+                .stream()
+                .findFirst();
+    }
+
+
+    public Set<String> getPrivileges(Set<String> roles) {
         Set<String> privileges;
         if (!CollectionUtils.isEmpty(roles)) {
             privileges = new HashSet<>();
@@ -77,4 +113,26 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
 
+    public static final String KEY_EMAIL = "email.value";
+    public static final String KEY_CONTACT = "contact.value";
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_ID = "_id";
+
+
+    public static final Map<String, String> KEY_MAPPINGS = new LinkedHashMap<>();
+    static {
+        KEY_MAPPINGS.put(Params.ID, KEY_ID);
+        KEY_MAPPINGS.put(Params.USERNAME, KEY_USERNAME);
+        KEY_MAPPINGS.put(Params.EMAIL, KEY_EMAIL);
+        KEY_MAPPINGS.put(Params.CONTACT, KEY_CONTACT);
+    }
+
+
+    public IRoleService getRoleService() {
+        return roleService;
+    }
+
+    public IUserService getUserService() {
+        return userService;
+    }
 }
