5f16da964bf297dce3496640116cd98270f3ae6a
Ankur Pathak <ankurpathak@live.in>
2018-10-24 05:39:29 +0530

    session control changes

---
+++ src/main/java/com/ankurpathak/springsessiontest/DispatcherRedirectStrategy.java
@@ -0,0 +1,33 @@
+package com.ankurpathak.springsessiontest;
+
+import org.springframework.security.web.DefaultRedirectStrategy;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DispatcherRedirectStrategy extends DefaultRedirectStrategy {
+
+
+    @Override
+    public void sendRedirect(HttpServletRequest request, HttpServletResponse response, String url) throws IOException {
+
+        String redirectUrl = calculateRedirectUrl(request.getContextPath(), url);
+        redirectUrl = response.encodeRedirectURL(redirectUrl);
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Redirecting to '" + redirectUrl + "'");
+        }
+        HttpServletRequest getRequest = new GetHttpServletRequestWrapper(request);
+        RequestDispatcher dispatcher = getRequest.getRequestDispatcher(redirectUrl);
+
+        try {
+            dispatcher.forward(getRequest, response);
+        } catch (ServletException ex) {
+            ex.printStackTrace();
+            throw new IOException(ex.getMessage(), ex);
+        }
+    }
+}
