52450b6d662e2f8ff0248b98f685e8e1a5942636
Ankur Pathak <ankurpathak@live.in>
2019-09-10 15:39:59 +0530

    all before test passed after migration for tests of non rest server

--- rest/src/test/java/com/github/ankurpathak/api/SpringSessionTestApplicationTests.java
+++ rest/src/test/java/com/github/ankurpathak/api/SpringSessionTestApplicationTests.java
@@ -1,13 +1,18 @@
 package com.github.ankurpathak.api;
 
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit.GreenMailRule;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 public class SpringSessionTestApplicationTests {
 
     @Test
     public void contextLoads() {
     }
-
 }
